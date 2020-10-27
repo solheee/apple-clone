928ecfb45f66d305e51ae685e2a50e64d2d99eb4
solheee <09merus@gmail.com>
2020-10-27 23:50:54 +0900

    html title 수정 js 업데이트

--- js/main.js
+++ js/main.js
@@ -108,6 +108,7 @@
             values: {
                 rect1X: [0, 0, { start: 0, end: 0 }],
                 rect2X: [0, 0, { start: 0, end: 0 }],
+                rectStartY: 0
             }
         }
     ];
@@ -316,6 +317,11 @@
                 // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
                 const recalculatedInnerWidth = window.innerWidth / canvasScaleRatio;
                 const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
+
+                if (!values.rectStartY) {
+                    values.rectStartY = objs.canvas.getBoundingClientRect();
+                    console.log(values.rectStartY);
+                }
 
                 const whiteRectWidth = recalculatedInnerWidth * 0.15;
                 values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
